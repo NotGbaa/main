b992495203a72517e9dff0268d23c0abdca0c6d3
MomoS216 <148048472+MomoS216@users.noreply.github.com>
2023-11-02 21:31:34 +0000

    cache ancora non finita

--- script1.js
+++ script1.js
@@ -3,6 +3,7 @@ const password = document.getElementById("password");
 const emailR = document.getElementById("emailR");
 const passwordR = document.getElementById("passwordR");
 const submit = document.getElementById("submit");
+const submit2 = document.getElementById("submit2");
 const indietro = document.getElementById("indietro");
 const registrati = document.getElementById("registrati");
 const div = document.getElementById("div");
@@ -10,15 +11,77 @@ const divR = document.getElementById("divR");
 const log = document.getElementById("log");
 const reg = document.getElementById("reg");
 const profili = [{ "email": "Admin", "password": "Admin" }];
+const loggedIn=true;
+const token = "4a318de6-2f15-4a2e-9161-a71279c9c016";
+
+
+function salvaDati(dict, key) {
+  fetch("https://ws.progettimolinari.it/cache/set", {
+    method: "Post",
+    headers: {
+      "content-type": "application/json",
+      key: token,
+    },
+    body: JSON.stringify({
+      key: key,
+      value: JSON.stringify(dict),
+    }),
+  })
+    .then((element) => element.json())
+    .then((element) => console.log(element))
+    .catch((error) => console.error(error));
+}
+
+
+function recuperaDati(chiave, dict) {
+  return new Promise((resolve, reject) => {
+    fetch("https://ws.progettimolinari.it/cache/get", {
+      method: "Post",
+      headers: {
+        "content-type": "application/json",
+        key: "32fdbbd2-ad29-46dc-996e-2db287ce3248",
+      },
+      body: JSON.stringify({ key: chiave }),
+    })
+      .then((element) => {
+        resolve(element.json());
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+salvaDati(profili, "profili");
+
+/*
+const render = () => {
+  if (loggedIn) {
+    log.classList.remove("visible");
+    log.classList.add("hidden");    
+    reg.classList.remove("hidden");
+    reg.classList.add("visible");
+  } else {
+    reg.classList.remove("visible");
+    reg.classList.add("hidden");
+    log.classList.remove("hidden");
+    log.classList.add("visible");        
+
+  }
+}
+render();
+*/
+
+
 
 // Nascondi il pulsante di submit inizialmente
 submit.style.display = "none";
+submit2.style.display = "none";
 reg.style.display = "none";
 divR.style.display = "none";
 
-// Aggiungi un ascoltatore di eventi su entrambi gli input
 email.addEventListener("input", toggleSubmitButton);
 password.addEventListener("input", toggleSubmitButton);
+emailR.addEventListener("input", toggleRegisterButton);
+passwordR.addEventListener("input", toggleRegisterButton);
 
 function toggleSubmitButton() {
   if (email.value !== "" && password.value !== "") {
@@ -28,17 +91,33 @@ function toggleSubmitButton() {
   }
 }
 
+function toggleRegisterButton() {
+  if (emailR.value !== "" && passwordR.value !== "") {
+    submit2.style.display = "block";
+  } else {
+    submit2.style.display = "none";
+  }
+}
+
 
 
 indietro.onclick = () => {
   log.style.display = "block";
   divR.style.display = "none";
   reg.style.display = "none";
+  submit2.style.display = "none";
   email.value = "";
   password.value = "";
 }
 
 submit.onclick = () => {
+
+  recuperaDati("profili").then((response) => {
+    console.log(JSON.parse(response.result));
+    profili = JSON.parse(response.result);
+  });
+
+  
   if (email.value === profili[0].email && password.value === profili[0].password) {
     window.location.href = "admin.html";
   }
@@ -51,18 +130,24 @@ submit.onclick = () => {
     //divR.style.display = "block";
     reg.style.display = "block";
     log.style.display = "none";
+    submit.style.display = "none";
     email.value = "";
     password.value = "";
   }
-  if (reg.style.display === "block" && log.style.display === "none" && emailR.value !== "" && passwordR !== "") {
+
+}
+
+submit2.onclick = () => {
+  if (emailR.value !== "" && passwordR.value !== "") {
     profili.push({ "email": emailR.value, "password": passwordR.value });
+    salvaDati(profili, "profili");
     indietro.click();
   }
 }
 
 
-
 registrati.onclick = () => {
   reg.style.display = "block";
   log.style.display = "none";
+  submit.style.display = "none";
 }
